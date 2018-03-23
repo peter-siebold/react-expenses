eff1b63115f6bd6870fa84c3c12f83d6a41e9429
peter-himself <peter.siebold@icloud.com>
2018-03-23 15:32:00 +0100

    added EditExpense Testcases

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -2,24 +2,36 @@ import React from "react";
 import {connect} from "react-redux";
 import {editExpense, removeExpense} from  "../actions/expenses";
 import ExpenseForm from "./ExpenseForm";
-const EditExpensePage = (props) => (
-    <div>
-        Edit expense for {props.match.params.id}
-        <ExpenseForm 
-            expense={props.expense}
-            onSubmit={(expense) => {
-                props.dispatch(editExpense(props.match.params.id, expense));
-                props.history.push("/");
-        }}/>
-        <button onClick={ () => {
-            props.dispatch(removeExpense({id: props.match.params.id}));
-            props.history.push("/");
-            } }>Remove</button>
-    </div>
-)
+
+export class EditExpensePage extends React.Component {
+    onSubmit = (expense) => {
+        this.props.editExpense(this.props.expense.id, expense);
+        this.props.history.push("/");
+    }
+    onRemove = () => {
+        this.props.removeExpense({id: this.props.expense.id});
+        this.props.history.push("/");
+    }
+    render() {
+        return(
+            <div>
+                Edit expense for {this.props.expense.id}
+                <ExpenseForm 
+                    expense={this.props.expense}
+                    onSubmit={this.onSubmit}/>
+                <button onClick={ this.onRemove }>Remove</button>
+            </div>
+        )
+    }
+}
+const mapDispatchToProps = (dispatch, props) => ({
+    editExpense: (id, expense) => dispatch(editExpense(id, expense)),
+    removeExpense: (data) => dispatch(removeExpense(data))
+})
+
 const mapStateToProps = (state, props) => {
     return {
         expense: state.expenses.find((expense) => expense.id === props.match.params.id)
     }
 }
-export default connect(mapStateToProps)(EditExpensePage);
+export default connect(mapStateToProps, mapDispatchToProps)(EditExpensePage);
---
+++ src/tests/components/EditExpensePage.test.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { EditExpensePage } from "../../components/EditExpensePage";
+import { shallow } from "enzyme";
+import expenses from "../fixtures/expenses";
+// import { editExpense, removeExpense } from "../../actions/expenses";
+
+
+let history, wrapper, match, editExpense, removeExpense;
+
+beforeEach(() => {
+
+    editExpense = jest.fn();
+    removeExpense = jest.fn();
+    history = {
+        push: jest.fn()
+    };
+    wrapper = shallow(<EditExpensePage 
+        editExpense={editExpense} 
+        removeExpense={removeExpense} 
+        history={history} 
+        expense={expenses[1]}
+        />);
+});
+
+test("should render AddExpensePage correctly", () => {
+    expect(wrapper).toMatchSnapshot();
+});
+
+test("should handle removeExpense", () => {
+    wrapper.find("ExpenseForm").prop("onSubmit")(expenses[1])
+    expect(history.push).toHaveBeenLastCalledWith("/");
+});
